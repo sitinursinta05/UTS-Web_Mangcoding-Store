46545e325e197da2b507775d27a2fba72a636b00
sitinursinta05 <74230146+sitinursinta05@users.noreply.github.com>
2022-11-13 14:11:55 +0700

    Delete mangcoding-store-master directory

--- mangcoding-store-master/app.js
+++
@@ -1,6 +0,0 @@
-const hamburgerButton = document.querySelector(".hamburger");
-const popup = document.querySelector(".popup");
-
-hamburgerButton.addEventListener("click", () => {
-  popup.classList.toggle("active");
-})
